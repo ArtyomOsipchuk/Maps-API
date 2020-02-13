dc17162878349c7ca758daf208bf58fad5035e22
Andrey-J <57663025+Andrey-J@users.noreply.github.com>
2020-02-13 17:22:35 +0300

    Add files via upload

--- Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API. Часть №1.py
@@ -1,26 +1,25 @@
+import os
+import sys
+
 import pygame
 import requests
-import os
 
-def render():
-    global spn, coords, map_file
-    response = None
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(str(i) for i in coords)}&spn={','.join(str(i) for i in spn)}&l=map"
-    response = requests.get(map_request)
-    # Запишем полученное изображение в файл.
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-    screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()
+response = None
+map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+response = requests.get(map_request)
 
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
 
-spn = (0.002, 0.002)
-coords = (37.530887, 55.703118)
-render()
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
@@ -29,21 +28,7 @@ screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_PAGEDOWN]:
-            if spn[0] + 0.5 <= 19:
-                spn = (spn[0] + 0.5, spn[1] + 0.5)
-                render()
-        if keys[pygame.K_PAGEUP]:
-            if spn[0] - 0.5 >= 0.0017:
-                spn = (spn[0] - 0.5, spn[1] - 0.5)
-                render()
-    pygame.display.flip()
 pygame.quit()
+
 # Удаляем за собой файл с изображением.
 os.remove(map_file)
